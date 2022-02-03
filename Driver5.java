83216260f1ecac9d0d73dfa1ad0ce7251de6299d
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:32:18 +0530

    Add files via upload

---
+++ Driver5.java
@@ -0,0 +1,21 @@
+package Dhanarjanudip01;
+class Driver5 {
+public static void main(String[] args)
+{
+int arr[] = { 11, 22, 33 };
+for (int i = 0; i < arr.length; i++)
+System.out.print(arr[i] + " ");
+System.out.println();
+int arr2[] = new int[3];
+//sint arr2[]= {11,22,33};
+arr2[0]=11;
+arr2[1]=22;
+arr2[2]=33;
+for (int i = 0; i < arr2.length; i++)
+System.out.print(arr2[i] + " ");
+}
+}
+
+//O/p
+//11 22 33 
+//11 22 33 
