9945733d385340c82b4b2ac88f51311efca3fa5a
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:31:55 +0530

    Add files via upload

---
+++ Driver4.java
@@ -0,0 +1,18 @@
+package Dhanarjanudip01;
+public class Driver4
+{ 
+public static void main ( String [] args ) 
+{ 
+int [] intArr = new int [5]; 
+for (int i = 0; i < intArr . length ; i ++) 
+{
+intArr [ i ] = i * 2;
+}
+for (int i = 0; i < intArr . length ; i ++) 
+{ 
+System . out . print ( intArr [ i ] + ", ") ;
+}}}
+
+
+//O/p
+//0, 2, 4, 6, 8, 
