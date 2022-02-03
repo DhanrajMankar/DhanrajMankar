3dd01f38d5d453a4362d3d6193eb08c3040eb2f4
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:33:56 +0530

    Add files via upload

---
+++ Drriver1.java
@@ -0,0 +1,22 @@
+package Dhanarjanudip01;
+public class Drriver1
+{ 
+public static void main ( String [] args ) 
+{ 
+double [] dblArr = {3.5 , 6.8 , 2.3 , 9.1 , 1.0};
+ for (int i = 0; i < dblArr . length ; i ++) 
+{
+ dblArr [ i ] /= 2; 
+} 
+for (int i = dblArr . length - 1; i >= 0; i--) 
+{ 
+System . out . println (" Value : " + dblArr [ i ]) ;
+ }
+}
+}
+//o/p
+//Value : 0.5
+//Value : 4.55
+//Value : 1.15
+//Value : 3.4
+//Value : 1.75
