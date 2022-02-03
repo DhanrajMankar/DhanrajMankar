01b88bef98194949ce9fc56557f6161e3b9942fe
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-03 23:30:25 +0530

    Add files via upload

---
+++ Driver3.java
@@ -0,0 +1,17 @@
+package Dhanarjanudip01;
+public class Driver3
+{ 
+public static void main ( String [] args )
+{ 
+int [] intArr = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
+for (int i = 0; i < intArr . length ; i += 3) 
+{ 
+System . out . println (" Value : " + intArr [ i ]) ; 
+} 
+} 
+}
+//O/p
+// Value : 1
+//Value : 4
+//Value : 7
+//Value: 10
