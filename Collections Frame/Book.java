9eb98393dbda1e52d8d93b32d29da81bf26fa6c6
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-02-10 23:44:13 +0530

    Add files via upload

---
+++ Collections Frame/Book.java
@@ -0,0 +1,52 @@
+package Dhanarjanudip01;
+import java.util.*; 
+class Book
+{
+ private int id;
+ private int quantity;
+ private String name;
+ private String author;
+ private String publisher;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}  
+	
+}  
+		
