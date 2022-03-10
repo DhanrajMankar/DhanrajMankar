8e88e086f8e559c2e745d86eecca75b3b0ed672f
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-03-10 13:34:34 +0530

    Add files via upload

---
+++ Threads/SynchronniseThread.java
@@ -0,0 +1,50 @@
+package Dhanraj03March;
+
+public class SynchronniseThread {
+	public static void main(String[]args) {
+      final print p=new print();
+      
+      Thread t=new Thread() {
+    	  public void run() {
+    		  p.display(5);}};
+    		  
+    		  Thread t1=new Thread() {
+    	    	  public void run() {
+    	    		  p.display(10);}};
+    	    		  
+    	    		  Thread t2=new Thread() {
+    	    	    	  public void run() {
+    	    	    		  p.display(15);}};
+    	    	    		  
+    	   t.start();
+    	   t1.start();
+    	   t2.start();
+	}
+
+}
+
+class print{
+   synchronized	void display(int num) {
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(num*i);
+			try {Thread.sleep(500);}
+			catch(Exception e) {
+				System.out.println(e);}	
+		}
+	}
+}
+class MyThread extends Thread{
+	print p;
+	MyThread(print p){
+		p.display(10);
+	}
+}
+class MyThread1 extends Thread{
+	print p;
+	MyThread1(print p){
+		p.display(20);
+	}
+}
+
+
