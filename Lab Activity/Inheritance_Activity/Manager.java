7ee8a570cd882bf4ea756f0e999105e355a59769
DhanrajMankar <96174796+DhanrajMankar@users.noreply.github.com>
2022-04-14 10:37:46 +0530

    Add files via upload

---
+++ Lab Activity/Inheritance_Activity/Manager.java
@@ -0,0 +1,39 @@
+package LabActivity;
+
+public class Manager extends Employee {
+    
+	double BasicSalary;
+     
+	
+		
+	public Manager(long employeeId, String employeeName, String employeeAdress, long empolyeePhone,
+			double basicSalary) {
+		super(employeeId, employeeName, employeeAdress, empolyeePhone);
+		BasicSalary = basicSalary;
+	}
+	
+
+
+
+	public void calculatesalary(double BasicSalary){
+		   
+		   double Salary;
+		   Salary=BasicSalary+(( BasicSalary * SpecialAllowance/100)+(BasicSalary*Hra/100));
+		   System.out.println("The total Salary is : "+Salary);
+	    	
+	    }
+	public void calculateTransportAllowance (double basicSalary) {
+	
+		
+		double transportAllowance = basicSalary*0.15;
+	
+		System.out.println("The Transport Allownace is : "+transportAllowance );
+	}
+	}
+
+	
+	
+
+
+
+
